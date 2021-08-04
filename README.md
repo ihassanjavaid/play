475a6d63f92fba21f75c561d7c888ae1f6bf258c
ihassanjavaid <great.maliks@gmail.com>
2021-08-05 00:10:59 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Play
+
+A hybrid mobile application made on flutter for video streaming.
+
+- Jazz Internship Task
+
+
---
+++ android/app/src/main/kotlin/com/jazzinternship/play/play_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.jazzinternship.play.play_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
